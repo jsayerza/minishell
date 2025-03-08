42b0e27152612a16b0dc4dbe9c3fceec8a5c1aeb
Andreu Carranza Gibert <acarranz@cbr4s1.42barcelona.com>
2025-03-08 12:24:52 +0100

    bucle de minishell

--- executor/start_shell.c
+++ executor/start_shell.c
@@ -12,20 +12,58 @@
 
 #include "../minishell.h"
 
+#include "minishell.h"
+
+char	*read_input(void)
+{
+	char	*input;
+	size_t	len;
+	ssize_t	read;
+
+	input = NULL;
+	len = 0;
+	read = getline(&input, &len, stdin);
+	if (read == -1)
+	{
+		free(input);
+		return (NULL);
+	}
+	if (input[read - 1] == '\n')
+		input[read - 1] = '\0';
+	return (input);
+}
+
 void	start_shell(t_shell *shell)
 {
+	char			*input;
+	t_constructor	*constructor;
 
-	// inicia bucle minishell
-	// aqui pleno de forma manual la llista
-	t_constructor *constructor = fill_constructor_manually();
-	if (!constructor)
+	while (1)
 	{
-		fprintf(stderr, "Error al crear el constructor.\n");
-		clean_shell(shell);
-		exit(1);
+		printf("~/minishell: ");
+		input = read_input();
+		if (!input)
+		{
+			printf("\n");
+			break ;
+		}
+		if (strcmp(input, "exit") == 0)
+		{
+			free(input);
+			break ;
+		}
+		constructor = fill_constructor_manually(input);
+		if (!constructor)
+		{
+			fprintf(stderr, "Error al crear el constructor.\n");
+			free(input);
+			clean_shell(shell);
+			exit(1);
+		}
+		shell->constructor = constructor;
+		print_constructor(shell);
+		display_shell(shell);
+		free(input);
 	}
-	shell->constructor = constructor;
-	print_constructor(shell);
-	display_shell(shell);
 	clean_shell(shell);
 }
