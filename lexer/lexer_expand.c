88005e9c0ce9400cdf96da1d50f20ea7427588c4
jsayerza <jsayerza.comptes@gmail.com>
2025-05-18 17:41:54 +0200

    parser: env

--- lexer/lexer_expand.c
+++ lexer/lexer_expand.c
@@ -246,6 +246,10 @@ void	tokens_expand(t_token **head,t_shell *shell, t_collector **collector)
 			curr->value = ft_strdup(expanded);
 			//freer(expanded);
 			collector_append(collector, curr->value);
+			// if (curr->type == TOKEN_COMMAND && is_assignment(curr->value))
+			// {
+			// 	curr->type = TOKEN_WORD;
+			// }			
 			tokens_print(head);
 			printf(" OUT tokens_expand TOKEN_WORD/COMMAND\n\n");
 		}
