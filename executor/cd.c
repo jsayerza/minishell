b24cae2d7f4be31c638f67f852549cb34d379916
acarranz <delregne00@github.com>
2025-06-04 19:07:12 +0200

    OLDPWD arreglat

--- executor/cd.c
+++ executor/cd.c
@@ -12,56 +12,82 @@
 
 #include "../minishell.h"
 
-void	cd_home(t_constructor *node)
+char *get_env_value(t_shell *shell, const char *key)
 {
-	char	*home;
+	int i;
+	size_t key_len;
 
-	home = get_home(node->shell);
+	if (!shell || !shell->env || !key)
+		return (NULL);
+	key_len = ft_strlen(key);
+	i = 0;
+	while (shell->env[i])
+	{
+		if (ft_strncmp(shell->env[i], key, key_len) == 0 && shell->env[i][key_len] == '=')
+			return (shell->env[i] + key_len + 1);
+		i++;
+	}
+	return (NULL);
+}
+
+void cd_home(t_constructor *node)
+{
+	char *home;
+
+	home = get_env_value(node->shell, "HOME");
+	if (!home)
+	{
+		printf("cd: HOME not set\n");
+		node->shell->last_exit = 1;
+		return;
+	}
 	printf("%s\n", home);
-	chdir(home);
+	if (chdir(home) == -1)
+	{
+		perror("cd");
+		node->shell->last_exit = 1;
+		return;
+	}
 	refresh_directori(node->shell, home, 1);
 }
 
-void	cd_oldpwd(t_constructor *node)
+void cd_oldpwd(t_constructor *node)
 {
-	char	*pwd;
+	char *oldpwd;
 
-	if (!node->shell->oldpwd) // Verifica si OLDPWD es NULL
-    {
-        printf("cd: OLDPWD not set\n");
-        node->shell->last_exit = 1; // Actualiza el código de salida
-        return ;
-    }
-    pwd = malloc((ft_strlen(node->shell->oldpwd) + 1) * sizeof(char));
-    if (!pwd)
-        return ;
-    strcpy(pwd, node->shell->oldpwd);
-    if (chdir(pwd) == 0)
-        refresh_directori(node->shell, pwd, 1);
-    else
-    {
-        printf("cd: %s: No such file or directory\n", pwd);
-        free(pwd);
-        node->shell->last_exit = 1;
-    }
+	oldpwd = get_env_value(node->shell, "OLDPWD");
+	if (!oldpwd)
+	{
+		printf("cd: OLDPWD not set\n");
+		node->shell->last_exit = 1;
+		return;
+	}
+	printf("%s\n", oldpwd);
+	if (chdir(oldpwd) == -1)
+	{
+		printf("cd: %s: No such file or directory\n", oldpwd);
+		node->shell->last_exit = 1;
+		return;
+	}
+	refresh_directori(node->shell, oldpwd, 1);
 }
 
-void	cd_directori(t_constructor *node)
+void cd_directori(t_constructor *node)
 {
 	if (chdir(node->executable[1]) == -1)
 	{
-		node->error = ERROR_NOT_ACCESS;
-		return ;
+		printf("cd: %s: No such file or directory\n", node->executable[1]);
+		node->shell->last_exit = 1;
+		return;
 	}
 	refresh_directori(node->shell, node->executable[1], 0);
 }
 
-void	cd(t_constructor *node)
+void cd(t_constructor *node)
 {
 	if (node->size_exec == 1)
 		cd_home(node);
-	else if (node->size_exec == 2
-		&& ft_strncmp(node->executable[1], "-", 1) == 0)
+	else if (node->size_exec == 2 && ft_strncmp(node->executable[1], "-", 1) == 0)
 		cd_oldpwd(node);
 	else
 		cd_directori(node);
--- executor/cd_utils01.c
+++ executor/cd_utils01.c
@@ -14,114 +14,135 @@
 
 void	copy_pwd_env(t_shell *shell, char *new_pwd, char *new_oldpwd)
 {
-	int		i;
+    int		i;
+    int		oldpwd_exists;
 
-	i = 0;
-	while (shell->env[i])
-	{
-		if (ft_strncmp(shell->env[i], "PWD=", 4) == 0)
-		{
-			free(shell->env[i]);
-			shell->env[i] = new_pwd;
-		}
-		else if (ft_strncmp(shell->env[i], "OLDPWD=", 7) == 0)
-		{
-			free(shell->env[i]);
-			shell->env[i] = new_oldpwd;
-		}
-		i++;
-	}
+    i = 0;
+    oldpwd_exists = 0;
+    while (shell->env[i])
+    {
+        if (ft_strncmp(shell->env[i], "PWD=", 4) == 0)
+        {
+            free(shell->env[i]);
+            shell->env[i] = new_pwd;
+        }
+        else if (ft_strncmp(shell->env[i], "OLDPWD=", 7) == 0)
+        {
+            free(shell->env[i]);
+            shell->env[i] = new_oldpwd;
+            oldpwd_exists = 1;
+        }
+        i++;
+    }
+
+    // Si OLDPWD no existe, agregarla al entorno
+    if (!oldpwd_exists)
+    {
+        char **new_env = malloc(sizeof(char *) * (i + 2));
+        if (!new_env)
+            return ;
+        i = 0;
+        while (shell->env[i])
+        {
+            new_env[i] = shell->env[i];
+            i++;
+        }
+        new_env[i] = new_oldpwd;
+        new_env[i + 1] = NULL;
+        free(shell->env);
+        shell->env = new_env;
+    }
 }
 
 void	update_environment(t_shell *shell)
 {
-	char	*new_pwd;
-	char	*new_oldpwd;
+    char	*new_pwd;
+    char	*new_oldpwd;
 
-	new_pwd = ft_strjoin("PWD=", shell->pwd);
-	new_oldpwd = ft_strjoin("OLDPWD=", shell->oldpwd);
-	if (!new_pwd || !new_oldpwd)
-	{
-		free(new_pwd);
-		free(new_oldpwd);
-		return ;
-	}
-	copy_pwd_env(shell, new_pwd, new_oldpwd);
+    new_pwd = ft_strjoin("PWD=", shell->pwd);
+    new_oldpwd = ft_strjoin("OLDPWD=", shell->oldpwd);
+    if (!new_pwd || !new_oldpwd)
+    {
+        free(new_pwd);
+        free(new_oldpwd);
+        return ;
+    }
+    copy_pwd_env(shell, new_pwd, new_oldpwd);
 }
 
 void	append_pwd_up(t_shell *shell)
 {
-	int		len;
-	char	*new_pwd;
+    int		len;
+    char	*new_pwd;
 
-	len = ft_strlen(shell->pwd);
-	while (len > 0 && shell->pwd[len - 1] != '/')
-		len--;
-	if (len > 1)
-		len--;
-	new_pwd = malloc((len + 1) * sizeof(char));
-	if (!new_pwd)
-		return ;
-	ft_strlcpy(new_pwd, shell->pwd, len + 1);
-	free(shell->oldpwd);
-	shell->oldpwd = shell->pwd;
-	shell->pwd = new_pwd;
-	printf("pwd->%s\n", shell->pwd);
-	printf("oldpwd->%s\n", shell->oldpwd);
-	update_environment(shell);
+    len = ft_strlen(shell->pwd);
+    while (len > 0 && shell->pwd[len - 1] != '/')
+        len--;
+    if (len > 1)
+        len--;
+    new_pwd = malloc((len + 1) * sizeof(char));
+    if (!new_pwd)
+        return ;
+    ft_strlcpy(new_pwd, shell->pwd, len + 1);
+    free(shell->oldpwd);
+    shell->oldpwd = shell->pwd;
+    shell->pwd = new_pwd;
+    printf("pwd->%s\n", shell->pwd);
+    printf("oldpwd->%s\n", shell->oldpwd);
+    update_environment(shell);
 }
 
 void	append_pwd(t_shell *shell, char *new_dir)
 {
-	char	*new_pwd;
-	char	*slash;
+    char	*new_pwd;
+    char	*slash;
 
-	if (ft_strcmp(new_dir, "..") == 0)
-	{
-		append_pwd_up(shell);
-		return ;
-	}
-	slash = ft_strjoin(shell->pwd, "/");
-	if (!slash)
-		return ;
-	new_pwd = ft_strjoin(slash, new_dir);
-	if (!new_pwd)
-		return ;
-	free(shell->oldpwd);
-	shell->oldpwd = shell->pwd;
-	shell->pwd = new_pwd;
-	collector_append(&shell->collector, new_pwd);
-	collector_append(&shell->collector, slash);
-	printf("pwd->%s\n", shell->pwd);
-	printf("oldpwd->%s\n", shell->oldpwd);
-	update_environment(shell);
+    if (ft_strcmp(new_dir, "..") == 0)
+    {
+        append_pwd_up(shell);
+        return ;
+    }
+    slash = ft_strjoin(shell->pwd, "/");
+    if (!slash)
+        return ;
+    new_pwd = ft_strjoin(slash, new_dir);
+    if (!new_pwd)
+        return ;
+    free(shell->oldpwd);
+    shell->oldpwd = shell->pwd;
+    shell->pwd = new_pwd;
+    collector_append(&shell->collector, new_pwd);
+    collector_append(&shell->collector, slash);
+    printf("pwd->%s\n", shell->pwd);
+    printf("oldpwd->%s\n", shell->oldpwd);
+    update_environment(shell);
 }
 
 void	refresh_directori(t_shell *shell, char *pwd, int type)
 {
-	char	*new_oldpwd;
-	int		i;
+    char	*new_oldpwd;
+    int		i;
 
-	new_oldpwd = malloc((ft_strlen(shell->pwd) + 1) * sizeof(char));
-	if (!new_oldpwd)
-		return ;
-	collector_append(&shell->collector, new_oldpwd);
-	if (!type)
-		append_pwd(shell, pwd);
-	else
-	{
-		i = 0;
-		while (shell->pwd[i])
-		{
-			new_oldpwd[i] = shell->pwd[i];
-			i++;
-		}
-		new_oldpwd[i] = '\0';
-		free(shell->oldpwd);
-		shell->oldpwd = new_oldpwd;
-		shell->pwd = pwd;
-		update_environment(shell);
-		printf("pwd->%s\n", shell->pwd);
-		printf("oldpwd->%s\n", shell->oldpwd);
-	}
+    new_oldpwd = malloc((ft_strlen(shell->pwd) + 1) * sizeof(char));
+    if (!new_oldpwd)
+        return ;
+    collector_append(&shell->collector, new_oldpwd);
+    if (!type)
+        append_pwd(shell, pwd);
+    else
+    {
+        i = 0;
+        while (shell->pwd[i])
+        {
+            new_oldpwd[i] = shell->pwd[i];
+            i++;
+        }
+        new_oldpwd[i] = '\0';
+        free(shell->oldpwd);
+        shell->oldpwd = new_oldpwd;
+        shell->pwd = pwd;
+        update_environment(shell);
+        printf("pwd->%s\n", shell->pwd);
+        printf("oldpwd->%s\n", shell->oldpwd);
+    }
 }
